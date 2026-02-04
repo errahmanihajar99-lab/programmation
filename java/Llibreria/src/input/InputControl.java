3221398c940ab644759a12c35d529e3c5410e853
errahmanihajar99-lab <errahmanihajar99@gmail.com>
2026-02-04 12:09:51 +0100

    rang

--- java/Llibreria/src/input/InputControl.java
+++ java/Llibreria/src/input/InputControl.java
@@ -5,11 +5,17 @@ import java.util.Scanner;
 public class InputControl {
     private static final Scanner scan = new Scanner(System.in);
 
-    // introduir un entier(missatge  est un paramettre qui permet d'ecrire n'importe quelle message
+    //1. introduir un entier(missatge  est un paramettre qui permet d'ecrire n'importe quelle message
     public static int introduirNumeroEnter(String missatge){
         System.out.println(missatge);
         int res = scan.nextInt();
         scan.hasNextLine();// pour viter le tampon pour eviter les bugs futurs
         return res;
+    }
+
+    //2.Introduire un numero verifiant qu'il est entre deux valeurs
+    public static boolean numeroRang(int min, int max){
+        int n = introduirNumeroEnter("entrodueix un numero");
+        return (n>min && n<max);
     }
 }
