7f495b614ba8f3db9e92c9d9baf9ac489bafb530
errahmanihajar99-lab <errahmanihajar99@gmail.com>
2026-02-04 12:32:25 +0100

    methode caratere

--- java/Llibreria/src/input/InputControl.java
+++ java/Llibreria/src/input/InputControl.java
@@ -7,6 +7,7 @@ public class InputControl {
 
     //1. introduir un entier(missatge  est un paramettre qui permet d'ecrire n'importe quelle message
     public static int introduirNumeroEnter(String missatge){
+
         System.out.println(missatge);
         int res = scan.nextInt();
         scan.hasNextLine();// pour viter le tampon pour eviter les bugs futurs
@@ -17,5 +18,23 @@ public class InputControl {
     public static boolean numeroRang(int min, int max){
         int n = introduirNumeroEnter("entrodueix un numero");
         return (n>min && n<max);
+    }
+
+
+    //3.saisie d'un caractere
+    public static char introduirCaracter(String missatge){
+        String s;
+        char c;
+
+        // si l utilisateur tape plus qu'un caracthere ou aucun on recommence la boucle
+        do {
+            System.out.println(missatge);
+            s = scan.nextLine();
+            scan.hasNextLine();
+        }while (s.length()!=1);
+
+            c = s.charAt(0);
+
+        return c;
     }
 }
