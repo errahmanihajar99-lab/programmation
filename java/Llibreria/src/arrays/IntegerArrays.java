d56b1ecf5e5ccd0ffe1e15b25c14c56b297b6e5a
errahmanihajar99-lab <errahmanihajar99@gmail.com>
2026-02-05 09:15:11 +0100

    les fanctions

--- java/Llibreria/src/arrays/IntegerArrays.java
+++ java/Llibreria/src/arrays/IntegerArrays.java
@@ -1,4 +1,23 @@
 package arrays;
+import java.util.Scanner;
 
 public class IntegerArrays {
+
+    // creation du tableau de n element
+    public static int[] crearArrayInt(int n){
+        return new int[n];
+    }
+
+    // creation du tableau bidimentonal
+    public static int[][] crearArrayintDid(int n, int m){
+        return new int[n][m];
+    }
+
+    // Initialiser avec une valeur donnée
+    public static void iniciArray(int[] tab, int v){
+        for (int i=0; i<tab.length; i++){
+            tab[i] = v;
+        }
+    }
+
 }
--- java/Llibreria/src/arrays/StringArrays.java
+++ java/Llibreria/src/arrays/StringArrays.java
@@ -1,4 +1,21 @@
 package arrays;
 
 public class StringArrays {
+
+    // creation du tableau de n element
+    public static String[] crearArrayStr(int n){
+        return new String[n];
+    }
+
+    // creation du tableau bidimentonal
+    public static String[][] crearArrayintDid(int n, int m){
+        return new String[n][m];
+    }
+
+    // Initialiser avec une valeur donnée
+    public static void iniciArrayBid(String[] tab, String v){
+        for (int i=0; i<tab.length; i++){
+            tab[i] = v;
+        }
+    }
 }
