80aae5113b8aaff615f42d8de28e85cbac6f76c0
errahmanihajar99-lab <errahmanihajar99@gmail.com>
2026-02-05 09:47:18 +0100

    les fanctions

--- java/Llibreria/src/arrays/IntegerArrays.java
+++ java/Llibreria/src/arrays/IntegerArrays.java
@@ -1,22 +1,37 @@
 package arrays;
-import java.util.Scanner;
+import java.util.Arrays;
+
 
 public class IntegerArrays {
 
-    // creation du tableau de n element
+    // 1.creation du tableau de n element
     public static int[] crearArrayInt(int n){
         return new int[n];
     }
 
-    // creation du tableau bidimentonal
+    // 2.creation du tableau bidimentonal
     public static int[][] crearArrayintDid(int n, int m){
         return new int[n][m];
     }
 
-    // Initialiser avec une valeur donnée
-    public static void iniciArray(int[] tab, int v){
+    // 3.Initialiser avec une valeur donnée
+    public static void iniciArrayInt(int[] tab, int v){
         for (int i=0; i<tab.length; i++){
             tab[i] = v;
+        }
+    }
+
+    // 4.Initialiser un array bidim avec une valeur donnée
+    public static void iniciArrayBidInt(int[][] tab, int v){
+        for (int[] ints : tab) {
+            Arrays.fill(ints, v);
+        }
+    }
+
+    // 5.Affichage des valeurs d'un tableau
+    public static void mostrarVArrayInt(int[] tab){
+        for (int j : tab) {
+            System.out.println(j);
         }
     }
 
--- java/Llibreria/src/arrays/StringArrays.java
+++ java/Llibreria/src/arrays/StringArrays.java
@@ -1,21 +1,37 @@
 package arrays;
 
+import java.util.Arrays;
+
 public class StringArrays {
 
-    // creation du tableau de n element
+    // 1.creation du tableau de n element
     public static String[] crearArrayStr(int n){
         return new String[n];
     }
 
-    // creation du tableau bidimentonal
+    // 2.creation du tableau bidimentonal
     public static String[][] crearArrayintDid(int n, int m){
         return new String[n][m];
     }
 
-    // Initialiser avec une valeur donnée
-    public static void iniciArrayBid(String[] tab, String v){
+    // 3.Initialiser avec une valeur donnée
+    public static void iniciArrayStr(String[] tab, String v){
         for (int i=0; i<tab.length; i++){
             tab[i] = v;
+        }
+    }
+
+    // 4.Initialiser un array bidim avec une valeur donnée
+    public static void iniciArrayBidStr(String[][] tab, String v){
+        for (String[] ligne : tab) {
+            Arrays.fill(ligne, v);
+        }
+    }
+
+    // 5.Affichage des valeurs d'un tableau
+    public static void mostrarVArrayStr(String[] tab){
+        for (String j : tab) {
+            System.out.println(j);
         }
     }
 }
