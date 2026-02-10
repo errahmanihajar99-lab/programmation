1141ba319b47188b5747c3ee7c9f748441836ffa
errahmanihajar99-lab <errahmanihajar99@gmail.com>
2026-02-10 09:59:36 +0100

    ajout d'une condition if pour afficher qui a gagner

--- java/TresEnRatlla/src/TresEnRatlla.java
+++ java/TresEnRatlla/src/TresEnRatlla.java
@@ -5,7 +5,7 @@ public class TresEnRatlla {
     public static char[][] joc = new char[3][3];
 
 
-    public static void main(String[] args) {
+     static void main(String[] args) {
         // remplissage du tableau
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
